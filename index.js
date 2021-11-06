9ed3681c6d0352d197b297661a0f74c87cf6d064
AlanRacciatti <alanracciatti1220@gmail.com>
2021-11-06 19:46:08 -0300

    fix

--- index.js
+++ index.js
@@ -1,68 +1,54 @@
-console.log("buenas")
-const express = require('express');
-const app = express();
+console.clear()
+// **** Require's ****
+const {Client, Intents} = require('discord.js');
+const {token, prefix, logo} = require('./config.json');
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Running server 3000")
-})
+// **** Bot setup ****
+const bot = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
-app.get("/", (req, res) => {
-    res.json({name: "name"})
-})
+bot.on("ready", () => {
+    console.log("Bot is ready");
+    bot.user.setActivity(`${prefix}help`, {type: "PLAYING"})
+    if (!bot.user.avatar === "https://storage.googleapis.com/assets.axieinfinity.com/axies/277561/axie/axie-full.png") {
+        bot.user.setAvatar("https://storage.googleapis.com/assets.axieinfinity.com/axies/277561/axie/axie-full.png")
+    }
+    bot.user.setUsername("Axie Bot")
+});
 
-// console.clear()
+// **** Commands ****
+const help = require('./commands/help');
+const myAxies = require('./commands/myAxies');
 
-// // **** Require's ****
-// const axios = require('axios').default;
-// const {Client, Intents} = require('discord.js');
-// const {token, prefix, logo} = require('./config.json');
+// **** Commands handler
+bot.on("messageCreate", (msg) => {
 
-// // **** Bot setup ****
-// const bot = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+    if (msg.author.bot) return;
+    if (!msg.content.startsWith(prefix)) return;
 
-// bot.on("ready", () => {
-//     console.log("Bot is ready");
-//     bot.user.setActivity(`${prefix}help`, {type: "PLAYING"})
-//     if (!bot.user.avatar === "https://storage.googleapis.com/assets.axieinfinity.com/axies/277561/axie/axie-full.png") {
-//         bot.user.setAvatar("https://storage.googleapis.com/assets.axieinfinity.com/axies/277561/axie/axie-full.png")
-//     }
-//     bot.user.setUsername("Axie Bot")
-// });
+    const commandBody = msg.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
 
-// // **** Commands ****
-// const help = require('./commands/help');
-// const myAxies = require('./commands/myAxies');
+    if (command === "help") {
 
-// // **** Commands handler
-// bot.on("messageCreate", (msg) => {
+        let data = {
+            msg: msg,
+            logo: logo
+        }
 
-//     if (msg.author.bot) return;
-//     if (!msg.content.startsWith(prefix)) return;
+        help(data)
+    } else if (command === "myaxies") {
 
-//     const commandBody = msg.content.slice(prefix.length);
-//     const args = commandBody.split(' ');
-//     const command = args.shift().toLowerCase();
+        let info = {
+            msg: msg,
+            args: args
+        }
 
-//     if (command === "help") {
+        myAxies(info)
 
-//         let data = {
-//             msg: msg,
-//             logo: logo
-//         }
+    }
 
-//         help(data)
-//     } else if (command === "myaxies") {
+});
 
-//         let info = {
-//             msg: msg,
-//             args: args
-//         }
-
-//         myAxies(info)
-
-//     }
-
-// });
-
-// // **** Login ****
-// bot.login(process.env.DJS_TOKEN);
+// **** Login ****
+bot.login(process.env.DJS_TOKEN);
