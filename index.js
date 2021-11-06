640209ee70eb543301615ad90b9132e296190d85
AlanRacciatti <alanracciatti1220@gmail.com>
2021-11-06 19:41:05 -0300

    update

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 console.log("buenas")
+const express = require('express');
+const app = express();
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Running server 3000")
+})
+
+app.get("/", (req, res) => {
+    res.json({name: "name"})
+})
 
 // console.clear()
 
