b8b48a54fe48d811d55abc51a9328dde9af531bc
AlanRacciatti <alanracciatti1220@gmail.com>
2021-11-06 20:29:50 -0300

    change normal embed color

--- embeds/normalEmbed.js
+++ embeds/normalEmbed.js
@@ -2,7 +2,7 @@ module.exports = (embedInfo) => {
     const { MessageEmbed } = require('discord.js');
     const normalEmbed = new MessageEmbed()
     .setTitle(embedInfo.title)
-    .setColor("DARK_RED")
+    .setColor("RED")
     .setDescription(embedInfo.description)
 
     return normalEmbed
