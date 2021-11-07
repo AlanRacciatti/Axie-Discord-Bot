e48f1926a8c09d987a3a1c9c4feba6c150336045
AlanRacciatti <alanracciatti1220@gmail.com>
2021-11-07 00:04:24 -0300

    fix help command

--- commands/help.js
+++ commands/help.js
@@ -5,8 +5,8 @@ module.exports = (data) => {
 	.setColor('#0099ff')
 	.setTitle('Axie Infinity Discord Bot')
 	.setDescription('Commands with functionalities I think would be useful, hope you like it :)')
-	.addField('$myaxies {ronin wallet}', 'It will show the whole Axies in this Ronin Account', true)
-	.addField('$myaccount {ronin wallet}', 'It will show info and stats of this Ronin Account', true)
+	.addField('-myaxies {ronin wallet}', 'It will show the whole Axies in this Ronin Account', true)
+	.addField('-myaccount {ronin wallet}', 'It will show info and stats of this Ronin Account', true)
 	.setTimestamp()
 
 data.msg.reply({ embeds: [helpEmbed] });
